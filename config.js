8c4d173a4b6461b0d0d6aa12182f10ddfe6d85f6
hafidha <hafidha.bouabdelli@gmail.com>
2026-09-23 18:50:11 +0100

    feature 06 new

--- config.js
+++ config.js
@@ -3,3 +3,5 @@ console.log("config 02");
 console.log("config 03");
 const x = 333;
 const y = 8;
+
+console.log("config 06");
--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ console.log("version 04");
 console.log("version 05");
 
 const y = 4;
+
+console.log("version 06");
