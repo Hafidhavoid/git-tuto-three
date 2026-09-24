166275172a807486d3b8eb434ed6bc9445133feb
hafidha <hafidha.bouabdelli@gmail.com>
2026-09-24 15:02:47 +0100

    version 07 index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let y = 8;
+let version = "version 07";
 console.log("version 02");
 console.log("version 03");
 console.log("version 04");
 console.log("version 05");
 
 const y = 4;
-console.log("version 06");
+console.log("version 07");
